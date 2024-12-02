c9b0a954bbf2076a76ffcabb737b887bb4da2d6c
Abderrezake <118079812+Abderrezake@users.noreply.github.com>
2024-12-02 21:31:46 +0100

    Add files via upload

---
+++ AppTest/src/main/java/com/abdou/AppTest/AppTestApplication.java
@@ -0,0 +1,14 @@
+package com.abdou.AppTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(AppTestApplication.class, args);
+	}
+
+}
